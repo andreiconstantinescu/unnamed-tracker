df6d13ffe95f3d8a359119ad34cc33732ffce261
Andrei Constantinescu <andrei.c@icloud.com>
2014-12-22 16:39:53 +0200

    Add server and scanner.

---
+++ scanner.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var ping = require('ping');
+var dns = require('dns');
+var when = require('when');
+var nodefn = require('when/node');
+var online = [];
+
+function reverseLookup(ip) {
+  return nodefn.call(dns.reverse, ip);
+}
+
+function testIp(ip) {
+  return when.promise(function (resolve, reject) {
+    ping.sys.probe(ip, function (online) {
+      resolve(online);
+    });
+  })
+}
+
+function toDecInt(elem) {
+  return parseInt(elem, 10);
+}
+
+function foreachIpIn(network, cb2) {
+  var n = [network];
+  var current;
+  var ip = n[0].split(".").map(function (ip) {
+    return parseInt(ip, 10);
+  });
+
+  for (var i = 2; i <= 254; i++) {
+    current = ip;
+    current[3] = i;
+    // console.log(current.join("."));
+    cb2(current.join("."));
+  }
+}
+
+module.exports = {
+  ping: function(network) {
+    var p = [];
+    foreachIpIn(network, function (ip) {
+      p.push(testIp(ip).then(function (exists) {
+        if (exists) {
+          var name = 'unnamed';
+          return reverseLookup(ip).then(function (domain) {
+            name = domain;
+          }).finally(function () {
+            online.push({
+              "ip": ip,
+              "name": name.toString(),
+            });
+          });
+        }
+      }));
+    });
+
+    return when.all(p).then(function () {
+      return online;
+    });
+  }
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var scanner = require('./scanner.js').ping('192.168.28.1');
+
+var app = express();
+
+var port = 1337;
+
+app.get('/', function (req, res) {
+  res.status(200);
+  scanner.then(function (users) {
+    res.send(users);
+    res.end();
+  })
+});
+
+app.listen(port);
+console.log('Server running at http://localhost:' + port);

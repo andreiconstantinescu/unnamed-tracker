ad53b1911c232e8aa9fd0eec53db1c95836c46f5
Andrei Constantinescu <andrei.c@icloud.com>
2014-12-29 18:28:40 +0200

    Convert RAW data.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var scanner = require('./lib/scanner').ping(process.env.NETWORK);
 var tracker = require('./lib/tracker');
+var clients = require('./lib/clients');
+var _ = require('lodash');
 
 var app = express();
 
@@ -12,22 +14,58 @@ var port = parseInt(process.env.PORT, 10) || 4000;
 
 app.use(express.static(__dirname + '/../public'));
 
-app.get('/', function (request, response) {
+app.get('/api/1.0/local', function (request, response) {
   response.status(200);
   scanner.then(function (users) {
     response.send(users);
+    _.each(users, function (user) {
+      console.log(user);
+    });
+  }).finally(function () {
     response.end();
   });
+
 });
+
+function findClient(id) {
+  return _.findKey(clients, {'togglId': id.toString()});
+}
+
+function convertDuration(seconds) {
+  var hours   = Math.floor(seconds / 3600);
+  var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+  var seconds = seconds - (hours * 3600) - (minutes * 60);
+
+  hours = hours   < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  var time = hours + ':' + minutes + ':' + seconds;
+  return time;
+}
 
 app.get('/api/1.0/current', function (request, response) {
   tracker.getData().then(function (data) {
     response.status(200);
-    response.send(data);
-    response.end();
+    var temp = JSON.parse(data);
+  _.each(temp, function(chunk) {
+    _.each(chunk, function (entry) {
+      entry.current = false;
+      entry.user_id = findClient(entry.user_id);
+      if (entry.duration < 0) {
+        var now = Math.floor(new Date().getTime() / 1000);
+        var then = Math.abs(entry.duration);
+        entry.duration = now - then;
+        entry.current = true;
+      }
+      entry.duration = convertDuration(entry.duration);
+    });
+  });
+    response.send(temp);
   }, function (err) {
     response.status(500);
     response.send(err.message);
+  }).finally(function() {
     response.end();
   });
 });

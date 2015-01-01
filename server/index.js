865c6af9855a64bdd77cc84c8d72f4356f3882f8
Andrei Constantinescu <andrei@constantinescu.io>
2015-01-01 23:14:13 +0300

    Minor.Nothing.

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ function findClient(id) {
 function convertDuration(seconds) {
   var hours   = Math.floor(seconds / 3600);
   var minutes = Math.floor((seconds - (hours * 3600)) / 60);
-  var seconds = seconds - (hours * 3600) - (minutes * 60);
+  seconds = seconds - (hours * 3600) - (minutes * 60);
 
   hours = hours   < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
